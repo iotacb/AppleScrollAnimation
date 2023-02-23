67689c86ce27063bf703366c467e3982e51e17a5
Chris <christopher.brandt09@gmail.com>
2023-02-23 16:35:22 +0100

    Fixed bug
    Frame Index could be wrong.
    Should be fixed by mapping the scroll progress to the image count

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const currentFrame = (index) => {
 
 const images = [];
 
+// Map a value from one range to another
+const map = (value, x1, y1, x2, y2) =>
+	((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+
 // Preload all images and store them in an array
 // This is done to prevent the images from flickering
 // And that images are not loaded multiple times
@@ -39,6 +43,7 @@ function doEffect() {
 	const updateImage = (index) => {
 		// Make sure the index is in the range of the array
 		if (index >= frameCount) return;
+
 		// Clear the canvas before drawing the next frame
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		// Draw the next frame
@@ -53,10 +58,10 @@ function doEffect() {
 		const scrollTop = html.scrollTop;
 		const maxScrollTop = html.scrollHeight - window.innerHeight;
 		const scrollFraction = scrollTop / maxScrollTop;
-		const frameIndex = Math.min(
-			frameCount,
-			Math.ceil(scrollFraction * frameCount)
-		);
+		const scrollProgress =
+			Math.round((scrollFraction + Number.EPSILON) * 100) / 100;
+		const frameIndex = Math.floor(map(scrollProgress, 0, 1, 1, frameCount - 1));
+
 		// Update the image on the canvas
 		updateImage(frameIndex);
 	});
